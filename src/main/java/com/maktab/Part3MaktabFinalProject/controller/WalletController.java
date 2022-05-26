0bd147daee09d4e481660598f33f21cf91413fc6
yousef.omd <yousefomidali78@gmail.com>
2022-05-26 22:36:10 +0430

    Added wallet feature and removed all warnings

--- src/main/java/com/maktab/Part3MaktabFinalProject/controller/WalletController.java
+++ src/main/java/com/maktab/Part3MaktabFinalProject/controller/WalletController.java
@@ -1,28 +1,38 @@
 package com.maktab.Part3MaktabFinalProject.controller;
 
+
+import com.maktab.Part3MaktabFinalProject.entity.UI.ChargeWallet;
+import com.maktab.Part3MaktabFinalProject.services.CustomerService;
 import com.maktab.Part3MaktabFinalProject.services.WalletService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping({"wallet","Wallet"})
+@RequestMapping({"wallet", "Wallet"})
 public class WalletController {
     private final WalletService walletService;
+    private final CustomerService customerService;
 
-    public WalletController(WalletService walletService) {
+    public WalletController(WalletService walletService, CustomerService customerService) {
         this.walletService = walletService;
+        this.customerService = customerService;
     }
 
-    @PostMapping({"charge","Charge"})
-    @ResponseBody
-    public String takeASubService(@RequestBody Long walletId, Long amount) {
-      var wallet = walletService.findById(walletId);
-      var walletCurrentAmount = wallet.getAmount();
-      wallet.setAmount((walletCurrentAmount+amount));
-      walletService.saveOrUpdate(wallet);
-        return "";
+    @GetMapping({"Save", "save", "Update", "update"})
+    public String saveOrUpdate(Model model) {
+        model.addAttribute("chargeWallet", new ChargeWallet());
+        return "chargeWallet";
+    }
+
+    @PostMapping({"Save", "save", "Update", "update"})
+    public String saveOrUpdate(@ModelAttribute("chargeWallet") ChargeWallet chargeWallet) {
+        var mainCustomer = customerService.findById(chargeWallet.getCustomerId());
+        var wallet = mainCustomer.getWallet();
+        var currentAmount = wallet.getAmount();
+        currentAmount += chargeWallet.getAmount();
+        wallet.setAmount(currentAmount);
+        walletService.saveOrUpdate(wallet);
+        return "success";
     }
 }
---
+++ src/main/java/com/maktab/Part3MaktabFinalProject/entity/UI/ChargeWallet.java
@@ -0,0 +1,15 @@
+package com.maktab.Part3MaktabFinalProject.entity.UI;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ChargeWallet {
+    private Long customerId;
+    private Long amount;
+}
